603da046de5921f8af40e2484b7a87abc9c0659e
Vladimir Novichikhin <darkonica@gmail.com>
2019-03-04 21:09:14 +0300

    Pack of improvements after [Travel landing] project

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-function hui() {
-  return hui;
+let hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', hamburgerToggle);
+
+function hamburgerToggle() {
+  this.classList.toggle('is-active');
+  document.querySelector('.mobile-nav').classList.toggle('is-active');
 }
